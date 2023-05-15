e9797894d21579a442719f3d28a7be9fa3c7a1bb
Zeeshan Shafique lap <zeeshaanshafique@gmail.com>
2023-05-16 01:40:53 +0500

    added basic documentation

--- CappellaAPI/CappellaAPI/settings.py
+++ CappellaAPI/CappellaAPI/settings.py
@@ -135,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_RESULTS_SERIALIZER = 'json'
+CELERY_TASK_SERIALIZER ='json'
